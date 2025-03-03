72cb8b764b1af8b4f1dd81ca26fe4036534fc8d5
Georgi Donchev <georgeuk26@gmail.com>
2025-03-03 18:25:03 +0000

    List user items in user table

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -131,7 +131,10 @@ export default function UserList() {
                         </tr>
                     </thead>
                     <tbody>
-                        <UserListItem />
+                        {users.map(user => <UserListItem 
+                           key={user._id} 
+                           {...user} 
+                        />)}
                     </tbody>
                 </table>
             </div>
