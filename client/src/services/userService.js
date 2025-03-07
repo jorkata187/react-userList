d2a5e21611b7b1b07a3e21689c4cf18e781ca301
Georgi Donchev <georgeuk26@gmail.com>
2025-03-07 22:47:39 +0000

    Fix post data

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -8,13 +8,20 @@ export default {
 
         return users;
     },
-    async create (userData) {
+    async create(userData) {
+
+        const { country, city, street, streetNumber, ...postData } = userData;
+
+        postData.address = { country, city, street, streetNumber };
+        postData.createdAt = new Date().toISOString();
+        postData.updatedAt = new Date().toISOString();
+
         const response = await fetch(baseUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(userData)
+            body: JSON.stringify(postData)
         });
 
         const result = await response.json();
